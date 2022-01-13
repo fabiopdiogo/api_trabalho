e42969bad379fe9501f2ac671896977435bc3e95
Fabio Diogo <fabindiogo@gmail.com>
2022-01-13 07:49:48 -0300

    correções no mapa

--- script/map.js
+++ script/map.js
@@ -1,107 +1,114 @@
-var map;
-function initMap(){
-    var mapOptions = {
-        center: {lat:-19.89033302336409, lng: -43.80103894819641},
-        zoom: 10,
-    };
+function initMap() {
+  const origin = {lat:-19.89033302336409, lng: -43.80103894819641};
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 18,
+    center: origin,
+  });
 
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  new ClickEventHandler(map, origin);
 
-    var marker = new google.maps.Marker ({
-        position: {lat:-19.84,lng:-43.880},
-        map: map,
-        title: 'Emile Coleta Lixo Êletronico',         
+  const tourStops = [
+    [{ lat: -19.809310670494128, lng: -43.875454924852136 }, "Boynton Pass"],
+    [{ lat: -19.84597450970574, lng: -43.87978540781596}, "Airport Mesa"],
+    [{ lat: -19.87421347285986, lng: -43.90259261807454}, "Chapel of the Holy Cross"],
+    [{ lat: -19.887788107203463, lng: -43.91009878854517}, "Red Rock Crossing"],
+    [{ lat: -19.8503192928919, lng: -43.91529536810176}, "Bell Rock"],
+  ];
+  // Create an info window to share between markers.
+  const infoWindow = new google.maps.InfoWindow();
+
+  // Create the markers.
+  tourStops.forEach(([position, title], i) => {
+    const marker = new google.maps.Marker({
+      position,
+      map,
+      title: `${title}`,      
+      optimized: false,
     });
 
-    let InfoWindow = new google.maps.InfoWindow({
-        content: '<h2>Emile Coleta Lixo Êletronico</h2>',
-        
-        //position: ,
-        //maxWidth: 200
+    // Add a click listener for each marker, and set up the info window.
+    marker.addListener("click", () => {
+      infoWindow.close();
+      infoWindow.setContent(marker.getTitle());
+      infoWindow.open(marker.getMap(), marker);
     });
-
-    marker.addListener('click',() => {
-        InfoWindow.open(map,marker);      
-    });
-
-    new ClickEventHandler(map, origin);
+  });
 }
 
-
 function isIconMouseEvent(e) {
-    return "placeId" in e;
+  return "placeId" in e;
+}
+
+class ClickEventHandler {
+  origin;
+  map;
+  directionsService;
+  directionsRenderer;
+  placesService;
+  infowindow;
+  infowindowContent;
+  constructor(map, origin) {
+    this.origin = origin;
+    this.map = map;
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsRenderer = new google.maps.DirectionsRenderer();
+    this.directionsRenderer.setMap(map);
+    this.placesService = new google.maps.places.PlacesService(map);
+    this.infowindow = new google.maps.InfoWindow();
+    this.infowindowContent = document.getElementById("infowindow-content");
+    this.infowindow.setContent(this.infowindowContent);
+    // Listen for clicks on the map.
+    this.map.addListener("click", this.handleClick.bind(this));
   }
-  
-  class ClickEventHandler {
-    origin;
-    map;
-    directionsService;
-    directionsRenderer;
-    placesService;
-    infowindow;
-    infowindowContent;
-    constructor(map, origin) {
-      this.origin = origin;
-      this.map = map;
-      this.directionsService = new google.maps.DirectionsService();
-      this.directionsRenderer = new google.maps.DirectionsRenderer();
-      this.directionsRenderer.setMap(map);
-      this.placesService = new google.maps.places.PlacesService(map);
-      this.infowindow = new google.maps.InfoWindow();
-      this.infowindowContent = document.getElementById("infowindow-content");
-      this.infowindow.setContent(this.infowindowContent);
-      // Listen for clicks on the map.
-      this.map.addListener("click", this.handleClick.bind(this));
-    }
-    handleClick(event) {
-      console.log("You clicked on: " + event.latLng);
-      // If the event has a placeId, use it.
-      if (isIconMouseEvent(event)) {
-        console.log("You clicked on place:" + event.placeId);
-        // Calling e.stop() on the event prevents the default info window from
-        // showing.
-        // If you call stop here when there is no placeId you will prevent some
-        // other map click event handlers from receiving the event.
-        event.stop();
-        if (event.placeId) {
-          this.calculateAndDisplayRoute(event.placeId);
-          this.getPlaceInformation(event.placeId);
-        }
+  handleClick(event) {
+    console.log("You clicked on: " + event.latLng);
+    // If the event has a placeId, use it.
+    if (isIconMouseEvent(event)) {
+      console.log("You clicked on place:" + event.placeId);
+      // Calling e.stop() on the event prevents the default info window from
+      // showing.
+      // If you call stop here when there is no placeId you will prevent some
+      // other map click event handlers from receiving the event.
+      event.stop();
+      if (event.placeId) {
+        this.calculateAndDisplayRoute(event.placeId);
+        this.getPlaceInformation(event.placeId);
       }
     }
-    calculateAndDisplayRoute(placeId) {
-      const me = this;
-  
-      this.directionsService
-        .route({
-          origin: this.origin,
-          destination: { placeId: placeId },
-          travelMode: google.maps.TravelMode.WALKING,
-        })
-        .then((response) => {
-          me.directionsRenderer.setDirections(response);
-        })
-        .catch((e) => window.alert("Directions request failed due to " + status));
-    }
-    getPlaceInformation(placeId) {
-      const me = this;
-  
-      this.placesService.getDetails({ placeId: placeId }, (place, status) => {
-        if (
-          status === "OK" &&
-          place &&
-          place.geometry &&
-          place.geometry.location
-        ) {
-          me.infowindow.close();
-          me.infowindow.setPosition(place.geometry.location);
-          me.infowindowContent.children["place-icon"].src = place.icon;
-          me.infowindowContent.children["place-name"].textContent = place.name;
-          me.infowindowContent.children["place-id"].textContent = place.place_id;
-          me.infowindowContent.children["place-address"].textContent =
-            place.formatted_address;
-          me.infowindow.open(me.map);
-        }
-      });
-    }
   }
+  calculateAndDisplayRoute(placeId) {
+    const me = this;
+
+    this.directionsService
+      .route({
+        origin: this.origin,
+        destination: { placeId: placeId },
+        travelMode: google.maps.TravelMode.WALKING,
+      })
+      .then((response) => {
+        me.directionsRenderer.setDirections(response);
+      })
+      .catch((e) => window.alert("Directions request failed due to " + status));
+  }
+  getPlaceInformation(placeId) {
+    const me = this;
+
+    this.placesService.getDetails({ placeId: placeId }, (place, status) => {
+      if (
+        status === "OK" &&
+        place &&
+        place.geometry &&
+        place.geometry.location
+      ) {
+        me.infowindow.close();
+        me.infowindow.setPosition(place.geometry.location);
+        me.infowindowContent.children["place-icon"].src = place.icon;
+        me.infowindowContent.children["place-name"].textContent = place.name;
+        me.infowindowContent.children["place-id"].textContent = place.place_id;
+        me.infowindowContent.children["place-address"].textContent =
+          place.formatted_address;
+        me.infowindow.open(me.map);
+      }
+    });
+  }
+}
